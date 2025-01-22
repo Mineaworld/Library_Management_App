a53d2a54029f0c8199301b6a5c6b01ed550371a6
Mineaworld <mineawebdev@gmail.com>
2025-01-22 15:47:54 +0700

    Enhance BookCard component with title, genre, and loan details display

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import BookCover from "./BookCover";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 const BookCard = ({
   id,
@@ -17,6 +19,27 @@ const BookCard = ({
       className={cn(isLoaned && "xs:w-52 w-full flex flex-col items-center")}
     >
       <BookCover coverColor={color} coverImage={coverUrl} />
+      <div className={cn("mt-4", !isLoaned && "xs:max-w-40 max-w-28")}>
+        <p className="book-title">{title}</p>
+        <p className="book-genre">{genre}</p>
+      </div>
+      {isLoaned && (
+        <div className="mt-3 w-full">
+          <div className="book-loaned">
+            <Image
+              src={"/icons/calendar.svg"}
+              alt="calendar"
+              width={18}
+              height={18}
+              className="object-contain"
+            />
+            <p className="text-light-100">7 days left</p>
+          </div>
+          <Button className="bg-dark-600 mt-3 min-h-14 w-full font-bebas-neue text-base text-primary hover:bg-dark-700 transition-colors duration-300 ">
+            Get receipt
+          </Button>
+        </div>
+      )}
     </Link>
   </li>
 );
