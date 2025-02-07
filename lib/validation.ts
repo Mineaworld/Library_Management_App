b376974ce12e0542bfcf29d113f35646e8813a86
Mineaworld <mineawebdev@gmail.com>
2025-02-07 14:07:03 +0700

    Add Input component and update SignUpSchema to include universityCard

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,9 +3,9 @@ import z from "zod";
 export const SignUpSchema = z.object({
   fullName: z.string().min(3),
   email: z.string().email(),
-  studentId: z.coerce.number(),
   password: z.string().min(8),
   confirmPassword: z.string().min(8),
+  studentId: z.coerce.number(),
   universityCard: z.string(),
 });
 
