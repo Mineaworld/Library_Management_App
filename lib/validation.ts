890ba6a69692b03deb1aef78ffd22ba8b070a8ae
Mineaworld <mineawebdev@gmail.com>
2025-02-07 13:58:02 +0700

    Rename uId field to studentId and add universityCard to SignUpSchema

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,9 +3,10 @@ import z from "zod";
 export const SignUpSchema = z.object({
   fullName: z.string().min(3),
   email: z.string().email(),
-  uId: z.coerce.number(),
+  studentId: z.coerce.number(),
   password: z.string().min(8),
   confirmPassword: z.string().min(8),
+  universityCard: z.string(),
 });
 
 export const SignInSchema = z.object({
