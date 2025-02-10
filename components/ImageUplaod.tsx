1cf072992d91f2e59ca0f8b0650f39da0b620c38
Mineaworld <mineawebdev@gmail.com>
2025-02-10 14:36:57 +0700

    Add ImageKit integration for image uploads; create config for API endpoints

--- components/ImageUplaod.tsx
+++ components/ImageUplaod.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+"use client";
 
+import config from "@/lib/config";
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+import { useRef, useState } from "react";
+
+const {
+  env: {
+    imagekit: { publicKey, urlEndpoint },
+  },
+} = config;
+
+const authenticator = async () => {
+  try {
+    const respone = await fetch(`${config.env.apiEndpint}/api/auth/imagekit`);
+
+    if (!respone.ok) {
+      const errortext = await respone.text();
+
+      throw new Error(
+        `Request failed with status ${respone.status} : ${errortext}`
+      );
+    }
+
+    const data = await respone.json();
+    const { token, expire, signature } = data;
+    return { token, expire, signature };
+  } catch (error: any) {
+    throw new Error(`Authecatication is failed : $error.message`);
+  }
+};
 const ImageUplaod = () => {
-  return <div>ImageUplaod</div>;
+  const IKuplaodRef = useRef(null);
+  const [file, setfile] = useState<{ filePath: string | null }>({
+    filePath: null,
+  });
+
+  return (
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload className="hidden" ref={IKuplaodRef} />
+    </ImageKitProvider>
+  );
 };
 
 export default ImageUplaod;
